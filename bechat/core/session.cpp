9eef600f38799f544dbbf4ecabb8ad3b695612c8
Keunlas <126337231+keunlas@users.noreply.github.com>
2026-08-16 01:31:06 +0800

    fix(Session): make session thread-safe

--- bechat/core/session.cpp
+++ bechat/core/session.cpp
@@ -4,7 +4,10 @@
 
 #include "bechat/utils/logger.h"
 
-Session::Session(asio::ip::tcp::socket socket) : socket_(std::move(socket)) {}
+Session::Session(asio::ip::tcp::socket socket)
+    : socket_(std::move(socket)),
+      read_strand_(asio::make_strand(socket_.get_executor())),
+      write_strand_(asio::make_strand(socket_.get_executor())) {}
 
 Session::~Session() { INFO("Session {} has closed.", (void*)this); }
 
@@ -17,12 +20,16 @@ void Session::read_tag() {
   auto self(shared_from_this());
   asio::async_read(
       socket_, asio::buffer(input_msg_.mutable_tag(), sizeof(TlvMessage::TagT)),
-      [this, self](std::error_code ec, std::size_t) {
-        if (!ec) {
-          input_msg_.set_tag(be16toh(input_msg_.tag()));
-          read_length();
-        }
-      });
+      asio::bind_executor(read_strand_,
+                          [this, self](std::error_code ec, std::size_t) {
+                            if (!ec) {
+                              input_msg_.set_tag(be16toh(input_msg_.tag()));
+                              read_length();
+                            } else {
+                              WARN("Error ocurred in Session {}, read_tag: {}",
+                                   (void*)this, ec.message());
+                            }
+                          }));
 }
 
 void Session::read_length() {
@@ -30,12 +37,16 @@ void Session::read_length() {
   asio::async_read(
       socket_,
       asio::buffer(input_msg_.mutable_length(), sizeof(TlvMessage::LengthT)),
-      [this, self](std::error_code ec, std::size_t) {
-        if (!ec) {
-          input_msg_.set_length(be16toh(input_msg_.length()));
-          read_value(input_msg_.length());
-        }
-      });
+      asio::bind_executor(
+          read_strand_, [this, self](std::error_code ec, std::size_t) {
+            if (!ec) {
+              input_msg_.set_length(be16toh(input_msg_.length()));
+              read_value(input_msg_.length());
+            } else {
+              WARN("Error ocurred in Session {}, read_length: {}", (void*)this,
+                   ec.message());
+            }
+          }));
 }
 
 void Session::read_value(uint16_t len) {
@@ -46,26 +57,62 @@ void Session::read_value(uint16_t len) {
   asio::async_read(
       socket_,
       asio::buffer(input_msg_.mutable_value()->data(), input_msg_.length()),
-      [this, self](std::error_code ec, std::size_t) {
+      asio::bind_executor(read_strand_, [this, self](std::error_code ec,
+                                                     std::size_t) {
         if (!ec) {
           TRACE("Session {} read a message: [0x{:x}][{}][{} bytes of value]",
                 (void*)this, input_msg_.tag(), input_msg_.length(),
                 input_msg_.value().size());
 
-          do_write();
-          read_tag();
+          write_resp();
+          read_tag();  // TCP 全双工通信，读写可同时进行
+        } else {
+          WARN("Error ocurred in Session {}, read_value: {}", (void*)this,
+               ec.message());
         }
-      });
+      }));
 }
 
-void Session::do_write() {
+void Session::write_resp() {
   auto self(shared_from_this());
   auto msg = std::make_shared<TlvMessage>(input_msg_);
+  asio::post(write_strand_, [this, self, msg]() {
+    write_queue_.push(std::move(msg));
+    if (!writing_) {
+      start_writing();
+    }
+  });
+}
+
+void Session::start_writing() {
+  assert(write_strand_.running_in_this_thread());
+
+  if (write_queue_.empty()) {
+    writing_ = false;
+    return;
+  }
+
+  writing_ = true;
+  auto self(shared_from_this());
+  auto msg = write_queue_.front();
+
   asio::async_write(
       socket_, asio::buffer(msg->value()),
-      [this, self, msg](std::error_code ec, std::size_t /*length*/) {
-        if (!ec) {
-          TRACE("do_write: done");
-        }
-      });
+      asio::bind_executor(
+          write_strand_,
+          [this, self, msg](std::error_code ec, std::size_t /*length*/) {
+            write_queue_.pop();
+            writing_ = false;
+
+            if (!ec) {
+              TRACE("writing a resp: done");
+              start_writing();
+            } else {
+              WARN(
+                  "Error ocurred in Session {}, start_writing: {}, "
+                  "Session is shutting down.",
+                  (void*)this, ec.message());
+              socket_.shutdown(asio::socket_base::shutdown_both);
+            }
+          }));
 }
--- bechat/core/session.h
+++ bechat/core/session.h
@@ -4,6 +4,7 @@
 #include <asio.hpp>
 #include <cstdint>
 #include <memory>
+#include <queue>
 
 #include "bechat/tlv/tlv_message.h"
 
@@ -19,11 +20,17 @@ class Session : public std::enable_shared_from_this<Session> {
   void read_length();
   void read_value(uint16_t len);
 
-  void do_write();
+  void write_resp();
+  void start_writing();
 
  private:
   asio::ip::tcp::socket socket_;
-  TlvMessage input_msg_;
+  asio::strand<asio::any_io_executor> read_strand_;
+  TlvMessage input_msg_{};
+
+  asio::strand<asio::any_io_executor> write_strand_;
+  std::queue<std::shared_ptr<TlvMessage>> write_queue_{};
+  bool writing_{false};
 };
 
 #endif  // !BECHAT_CORE_SESSION_H_
